5405ee3e65001779fd6d2702adf93e628aafe8a1
rinda <rk662@njit.edu>
2018-03-28 17:17:33 -0400

    13. Created a unit test to insert a user into the users table.

---
+++ tests/Unit/InsertTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class InsertTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $user=new User();
+        $user->name='rinda';
+        $user->email='rk@njit.edu';
+        $user->password='secret';
+        $this->assertTrue($user->save());
+    }
+}
